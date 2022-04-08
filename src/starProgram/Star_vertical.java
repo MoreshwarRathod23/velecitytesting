d00d00f02db1dc619c92291404550c5bbab069f8
MoreshwarRathod23 <msrathod221@gmail.com>
2022-04-08 21:52:33 +0530

    this is 1st commit

---
+++ src/starProgram/Star_vertical.java
@@ -0,0 +1,16 @@
+package starProgram;
+
+public class Star_vertical {
+//	*	
+//	*
+//	*
+//	*
+//	*
+	public static void main(String[] args) {
+	for (int i=1; i<=4; i++)
+	{System.out.println("*");
+		
+	}
+	}
+
+}
